f032933c41a23a8b7ef2f53cadb9af685735e995
meysamzamani <meysamzamani@gmail.com>
2023-09-17 19:45:35 +0200

    style: define base of project

---
+++ src/test/java/com/meysamzamani/PopularRepoFinder/PopularRepoFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meysamzamani.PopularRepoFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularRepoFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
